9a8dac8bec198ea7b923fe398af3d89ca061629a
Hoàng Phú <63839574+PhuNguyen01997@users.noreply.github.com>
2020-11-12 17:22:25 +0700

    commit

--- js/timeScript.js
+++ js/timeScript.js
@@ -3,18 +3,24 @@ $(document).ready(function () {
     const secSpan = $(".compass-clock__item--second");
     const minSpan = $(".compass-clock__item--minute");
     const hourSpan = $(".compass-clock__item--hour");
+    const daySpan = $(".compass-clock__item--day");
+    const monthSpan = $(".compass-clock__item--month");
+    const weekSpan = $(".compass-clock__item--week");
     set60(secSpan, date.getSeconds());
     set60(minSpan, date.getMinutes()); // Because both second and minute have 60 number, so i use one function
     set24(hourSpan, date.getHours());
+    set30(daySpan, date.getDate());
+    set12(monthSpan, date.getMonth() + 1);
+    set4(weekSpan, getWeek(date));
     const eachSecond = setInterval(() => {
         date = new Date();
         incre60(secSpan, date.getSeconds());
         incre60(minSpan, date.getMinutes());
-    }, 1000);
-    const eachMinute = setInterval(() => {
-        date = new Date();
         incre24(hourSpan, date.getHours());
-    }, 1000*60);
+        incre30(daySpan, date.getDate());
+        incre12(monthSpan, date.getMonth() + 1);
+        incre4(weekSpan, getWeek(date));
+    }, 1000);
 });
 
 function setTransition(nodeList) {
@@ -25,7 +31,7 @@ function setTransition(nodeList) {
 
 function set60(nodeList, second) {
     $.each(nodeList, function (index, elem) {
-        deg = parseInt(elem.dataset.value);
+        let deg = parseInt(elem.dataset.value);
         deg = (second - deg) * 6; // (360 / 60)
         $(elem).css("transform", "rotate(" + deg + "deg)");
     });
@@ -33,37 +39,99 @@ function set60(nodeList, second) {
     // Nếu thêm transition ngay từ đầu thì vừa load trang xong sẽ bu từ 1 cục r tản ra
     // After set correct position, set transition
     // Có thể sử dụng promise mà k cần sử dụng setTimeout, mục đích chính là sau khi sét giờ xong thì nhét transition vào
-    setTimeout(()=>{
+    setTimeout(() => {
         setTransition(nodeList);
-    },500);
+    }, 500);
 
 }
 
 function incre60(nodeList, second) {
     $.each(nodeList, function (index, elem) {
-        deg = parseInt(elem.dataset.value);
+        let deg = parseInt(elem.dataset.value);
         deg = (second - deg) * 6; // (360 / 60)
         $(elem).css("transform", "rotate(" + deg + "deg)");
     });
 }
 
-function set24(nodeList, hour){
-    $.each(nodeList, function(index, elem){
-        deg = parseInt(elem.dataset.value);
+function set24(nodeList, hour) {
+    $.each(nodeList, function (index, elem) {
+        let deg = parseInt(elem.dataset.value);
         deg = (hour - deg) * 15; // (360 / 24);
         $(elem).css("transform", "rotate(" + deg + "deg)");
-        console.log(elem)
     });
 
-    setTimeout(()=>{
+    setTimeout(() => {
         setTransition(nodeList);
-    },500);
+    }, 500);
 }
 
-function incre24(nodeList, second) {
+function incre24(nodeList, hour) {
     $.each(nodeList, function (index, elem) {
-        deg = parseInt(elem.dataset.value);
+        let deg = parseInt(elem.dataset.value);
         deg = (hour - deg) * 15; // (360 / 24);
         $(elem).css("transform", "rotate(" + deg + "deg)");
     });
+}
+
+function set30(nodeList, day) {
+    $.each(nodeList, function (index, elem) {
+        let deg = parseInt(elem.dataset.value);
+        deg = (day - deg) * 12; // (360 / 30);
+        $(elem).css("transform", "rotate(" + deg + "deg)");
+    });
+
+    setTimeout(() => {
+        setTransition(nodeList);
+    }, 500);
+}
+
+function incre30(nodeList, day) {
+    $.each(nodeList, function (index, elem) {
+        let deg = parseInt(elem.dataset.value);
+        deg = (day - deg) * 12; // (360 / 30);
+        $(elem).css("transform", "rotate(" + deg + "deg)");
+    });
+}
+
+function set12(nodeList, month) {
+    $.each(nodeList, function (index, elem) {
+        let deg = parseInt(elem.dataset.value);
+        deg = (month - deg) * 30; // (360 / 12);
+        $(elem).css("transform", "rotate(" + deg + "deg)");
+    });
+
+    setTimeout(() => {
+        setTransition(nodeList);
+    }, 500);
+}
+
+function incre12(nodeList, month) {
+    $.each(nodeList, function (index, elem) {
+        let deg = parseInt(elem.dataset.value);
+        deg = (month - deg) * 30; // (360 / 12);
+        $(elem).css("transform", "rotate(" + deg + "deg)");
+    });
+}
+
+function set4(nodeList, week) {
+    $.each(nodeList, function (index, elem) {
+        let deg = parseInt(elem.dataset.value);
+        deg = (week - deg) * 30; // (360 / 12);
+        $(elem).css("transform", "rotate(" + deg + "deg)");
+    });
+}
+
+function incre4(nodeList, week) {
+    $.each(nodeList, function (index, elem) {
+        let deg = parseInt(elem.dataset.value);
+        deg = (week - deg) * 30; // (360 / 12);
+        $(elem).css("transform", "rotate(" + deg + "deg)");
+    });
+}
+
+function getWeek(date) {
+    let monthStart = new Date(date);
+    monthStart.setDate(0);
+    let offset = (monthStart.getDay() + 1) % 7 - 1; // -1 is for a week starting on Monday
+    return Math.ceil((date.getDate() + offset) / 7);
 }
